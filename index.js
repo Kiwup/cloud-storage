a2e46458f7824d862cbe40584689e4d2a59a478a
Frantz Gauthier <frantz@myentropy.org>
2017-06-09 11:18:07 +0200

    feat Add Google Storage adapter with upload method

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict'
 
 const s3 = require('./lib/s3')
+const googleStorage = require('./lib/google_storage')
 
 function adapter (provider) {
   if (provider === 's3') {
     return s3
+  } else if (provider === 'googleStorage') {
+    return googleStorage
   }
 }
 
 adapter.s3 = s3
+adapter.googleStorage = googleStorage
 
 exports = module.exports = adapter
---
+++ test/e2e/google_storage.e2e.js
@@ -0,0 +1,132 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const test = require('tape')
+const { googleStorage } = require('../../.')
+const {
+  uploadPublic,
+  uploadPublicWithContentType,
+  uploadRestricted
+} = require('./upload')
+
+const { GCP_PROJECT_ID, GCP_REGION } = process.env
+
+if (!GCP_PROJECT_ID || !GCP_REGION) {
+  throw new Error('Missing env vars GCP_PROJECT_ID, GCP_REGION')
+}
+
+const samplePath = path.join(__dirname, 'sample.json')
+const testObject = 'test2'
+
+const storage = googleStorage({
+  projectId: GCP_PROJECT_ID,
+  keyFilename: './test/e2e/gcp_key.json'
+})
+
+function setup (testBucket, t) {
+  t.test('create test bucket', (t) => {
+    // native google API
+    storage.client.createBucket(testBucket, {
+      regional: true,
+      location: GCP_REGION
+    }, t.end)
+  })
+}
+
+function teardown (testBucket, t) {
+  const bucket = storage.client.bucket(testBucket)
+  t.test('delete test object', (t) => {
+    // native google API
+    bucket.deleteFiles(t.end)
+  })
+  t.test('remove test bucket', (t) => {
+    // native google API
+    bucket.delete(t.end)
+  })
+}
+
+test('Google > upload a buffer with public access', (t) => {
+  const testBucket = `cloud-storage-e2e-${Date.now()}`
+  setup(testBucket, t)
+  t.test('upload a buffer', (t) => {
+    fs.readFile(samplePath, (err, data) => {
+      if (err) {
+        return t.end(err)
+      }
+      uploadPublic(storage, testBucket, testObject, t, data)
+    })
+  })
+  teardown(testBucket, t)
+})
+
+test('Google > upload a buffer with restricted access', (t) => {
+  const testBucket = `cloud-storage-e2e-${Date.now()}`
+  setup(testBucket, t)
+
+  t.test('upload a buffer', (t) => {
+    fs.readFile(samplePath, (err, data) => {
+      if (err) {
+        return t.end(err)
+      }
+      uploadRestricted(storage, testBucket, testObject, t, data)
+    })
+  })
+
+  teardown(testBucket, t)
+})
+
+test('Google > upload a buffer publicly and specifying content-type', (t) => {
+  const testBucket = `cloud-storage-e2e-${Date.now()}`
+  setup(testBucket, t)
+
+  t.test('upload a buffer', (t) => {
+    fs.readFile(samplePath, (err, data) => {
+      if (err) {
+        return t.end(err)
+      }
+      uploadPublicWithContentType(storage, testBucket, testObject, t, data)
+    })
+  })
+
+  teardown(testBucket, t)
+})
+
+test('Google > upload a stream with public access', (t) => {
+  const testBucket = `cloud-storage-e2e-${Date.now()}`
+  setup(testBucket, t)
+
+  t.test('upload a stream', (t) => {
+    const data = fs.createReadStream(samplePath)
+    data.on('error', t.end)
+    uploadPublic(storage, testBucket, testObject, t, data)
+  })
+
+  teardown(testBucket, t)
+})
+
+test('Google > upload a stream with restricted access', (t) => {
+  const testBucket = `cloud-storage-e2e-${Date.now()}`
+  setup(testBucket, t)
+
+  t.test('upload a stream', (t) => {
+    const data = fs.createReadStream(samplePath)
+    data.on('error', t.end)
+    uploadRestricted(storage, testBucket, testObject, t, data)
+  })
+
+  teardown(testBucket, t)
+})
+
+test('Google > upload a stream publicly and specifying content-type', (t) => {
+  const testBucket = `cloud-storage-e2e-${Date.now()}`
+  setup(testBucket, t)
+
+  t.test('upload a stream', (t) => {
+    const data = fs.createReadStream(samplePath)
+    data.on('error', t.end)
+    uploadPublicWithContentType(storage, testBucket, testObject, t, data)
+  })
+
+  teardown(testBucket, t)
+})
